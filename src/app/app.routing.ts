2be046a40a2c2a41ca05d118aac0c89c567f1c1e
shalu chandwani <48604142+shalu93@users.noreply.github.com>
2023-03-21 00:11:05 +0200

    removed profile

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,13 +4,11 @@ import { BrowserModule  } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
-import { ProfileComponent } from './profile/profile.component';
 import { RequestDemoComponent } from './requestDemo/requestDemo.component';
 import { GetStartedComponent } from './getStarted/getStarted.component';
 
 const routes: Routes = [
     { path: 'home',             component: HomeComponent },
-    { path: 'user-profile',     component: ProfileComponent },
     { path: 'request-demo',           component: RequestDemoComponent },
     { path: 'get-started',          component: GetStartedComponent },
     { path: '', redirectTo: 'home', pathMatch: 'full' }
